a4253df87e6aaf4807577632092c2c2fa03fc559
t-aono <info@t-aono.net>
2022-05-16 09:05:17 +0900

    create selectors file

--- src/templates/Home.jsx
+++ src/templates/Home.jsx
@@ -1,8 +1,14 @@
+import { useSelector } from "react-redux";
+import { getUserId } from "../reducks/users/selectors";
 
 const Home = () => {
+  const selector = useSelector(state => state);
+  const uid = getUserId(selector);
+
   return (
     <div>
       <h2>Home</h2>
+      <p>{uid}</p>
     </div>
   )
 }
--- src/templates/Login.jsx
+++ src/templates/Login.jsx
@@ -1,12 +1,17 @@
 import { useDispatch } from "react-redux";
 import { push } from 'connected-react-router';
+import { signInAction } from "../reducks/users/actions";
 
 const Login = () => {
   const dispatch = useDispatch();
   return (
     <div>
       <h2>ログイン</h2>
-      <button onClick={() => dispatch(push('/'))}>
+      <button onClick={() => {
+        dispatch(signInAction({ uid: "0001", username: "Tom " }))
+        dispatch(push('/'));
+      }
+      }>
         ログイン
       </button>
     </div>
